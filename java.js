96e7b75fdb03958dc4d1054b305b87b0c1eeb6a8
Mirzokhidd <50044566+Mirzokhidd@users.noreply.github.com>
2020-09-06 20:24:56 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+function openStatueBox() {
+    document.getElementById("statue").style.display = "block";
+};
+
+function closeStatueBox() {
+    document.getElementById("statue").style.display = "none";
+};
+
+function openVaseBox() {
+    document.getElementById("vase").style.display = "block";
+};
+
+function closeVaseBox() {
+    document.getElementById("vase").style.display = "none";
+};
+
